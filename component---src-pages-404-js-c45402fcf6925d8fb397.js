732e54d3597e12525f20a03fd0faef1d0e7a26f6
vigneshgowrabathuni <vgowrabathuni@gmail.com>
2018-12-14 21:59:28 +0530

    added portfolio data

---
+++ component---src-pages-404-js-c45402fcf6925d8fb397.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{131:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),u=n(136),s=n(138);t.default=function(){return l.a.createElement(u.d,null,l.a.createElement(s.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c45402fcf6925d8fb397.js.map
