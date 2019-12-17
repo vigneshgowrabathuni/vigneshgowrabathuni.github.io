44f6880d9991e3d78066d02e556e2048c27415cd
vigneshgowrabathuni <vgowrabathuni@gmail.com>
2019-12-17 20:26:54 +0530

    Updated resume url

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-08eac4846cc71a3914e6.js"
+    "url": "webpack-runtime-3a9fbe7869e48239d124.js"
   },
   {
     "url": "app-e4500ea5f7d6689b3a37.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dc6916df745f068a2a099a9dd8c22292"
+    "revision": "7e47a3c7ab5b3d4835f844d39f570452"
   },
   {
     "url": "styles.c4ac7a04f2b8f1c8facb.css"
